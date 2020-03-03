594d88b3ccd091694aa5ce5f717da4d0751c9e55
Chunchun Kumar <chunchun_kumar@persistent.com>
2020-03-03 10:47:26 +0530

    created new project

---
+++ udemy-spring-boot/src/main/java/com/psl/udemyspringboot/UdemySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.psl.udemyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemySpringBootApplication.class, args);
+	}
+
+}
---
+++ udemy-spring-boot/src/test/java/com/psl/udemyspringboot/UdemySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psl.udemyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
